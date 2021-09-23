9c7ac62c78b3786a9f9132697ba47d69708f8000
Sam <63341022+whereissam@users.noreply.github.com>
2021-09-23 17:35:58 +0800

    Class

---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student s1 = new Student(1020, "ha");
+            Student s2 = new Student(1021, "JA");
+            Student s3 = new Student(2020, "wow", 3);
+
+            MessageBox.Show(s2.Say());
+        }
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace WinFormsApp1
+{
+    class Student
+    {
+        public int StudentID;
+        public string Name;
+        public int Grade;
+
+        //Method
+        //public output型別 method名稱(Input型別與名稱)
+        public Student(int studentID, string name)
+        {
+            StudentID = studentID;
+            Name = name;
+            Grade = 1;
+        }
+
+        public Student(int studentID, string name, int grade)
+        {
+            StudentID = studentID;
+            Name = name;
+            Grade = grade;
+        }
+
+        public string Say()
+        {
+            return "I am " + Name + " ,I am " + Grade + " grade student";
+        }
+
+        public string Talk(Student s2)
+        {
+            return Grade + " grade "+ Name + " ,"+ s2.Grade + " grade " + s2.Name + " say hello ";
+        }
+
+        public void Upgrade()
+        {
+            Grade++;
+        }
+    }
+}
