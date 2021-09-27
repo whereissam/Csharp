f62bcbb77488bbf16fd7ed9510823f1bd8be7407
Sam <63341022+whereissam@users.noreply.github.com>
2021-09-27 11:15:27 +0800

    switch & enum

---
+++ Switch & enum/Monster.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinFormsApp1
+{
+    class Monster
+    {
+        private int X;
+        private int Y;
+
+        public void Move(Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.UP:
+                    Y += 1;
+                    break;
+                case Direction.DOWN:
+                    Y -= 1;
+                    break;
+                case Direction.LEFT:
+                    X -= 1;
+                    break;
+                case Direction.RIGHT:
+                    X += 1;
+                    break;
+                default:
+                    break;
+            }
+            /*
+            if(direction == 1)
+            {
+                Y += 1;
+            } else if(direction == 2)
+            {
+                Y -= 1;
+            } else if (direction == 3)
+            {
+                X -= 1;
+            } else if (direction == 4)
+            {
+                X += 1;
+            }
+            */
+        }
+        public string ReportPosition()
+        {
+            return "怪物在(" + X + "," + Y + ")";
+        }
+
+    }
+}
