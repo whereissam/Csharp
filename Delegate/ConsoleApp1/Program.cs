583ea9dc31b4afc81eaf0afa87c1a98828d2aa11
Sam <63341022+whereissam@users.noreply.github.com>
2021-10-08 10:24:23 +0800

    Delegate - Func / Action

---
+++ Delegate/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductFactory productFactory = new ProductFactory();
+            WrapFactory wrapFactory = new WrapFactory();
+
+            Func<Product> func1 = new Func<Product>(productFactory.MakePizza);
+            //MakePizza 為 product, Func<Product> 也為 product 類型相同因此可以調用
+            Func<Product> func2 = new Func<Product>(productFactory.MakeToyCar);
+
+            Logger logger = new Logger();
+            Action<Product> log = new Action<Product>(logger.Log);
+
+            Box box1 = wrapFactory.WrapProduct(func1, log);
+            Box box2 = wrapFactory.WrapProduct(func2, log);
+
+
+            Console.WriteLine(box1.Product.Name);
+            Console.WriteLine(box2.Product.Name);
+        }
+    }
+
+    class Logger
+    {
+        public void Log(Product product)
+        {
+            Console.WriteLine("Product '{0}' created at {1}. Price is {2}. ", product.Name, DateTime.UtcNow, product.Price);
+        }
+    }
+
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+
+    class Box
+    {
+        public Product Product { get; set; }
+    }
+
+    class WrapFactory
+    {
+        public Box WrapProduct(Func<Product> getProduct, Action<Product> logCallback)
+        {
+            Box box = new Box();
+            Product product = getProduct.Invoke();
+            if(product.Price >= 50)
+            {
+                logCallback(product);
+            }
+
+            box.Product = product;
+            return box;
+        }
+    }
+
+    class ProductFactory
+    {
+        public Product MakePizza()
+        {
+            Product product = new Product();
+            product.Name = "Pizza";
+            product.Price = 12;
+            return product;
+        }
+
+        public Product MakeToyCar()
+        {
+            Product product = new Product();
+            product.Name = "Toy car";
+            product.Price = 120;
+            return product;
+        }
+    }
+}
